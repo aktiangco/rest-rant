7e46cfd3f7f909a199007ea656f47ed46bb21a31
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-01-23 18:05:42 -0800

    Adding places view with mock data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,6 +6,9 @@ function index (data) {
     return (
       <div>
         <h2>{place.name}</h2>
+        <p>{place.city}</p>
+        <p>{place.state}</p>
+        <p>{place.cuisines}</p>
         <img src={place.pic} alt={place.name}/>
       </div>
     )
