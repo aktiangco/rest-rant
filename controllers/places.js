7a1d1e90ff167636a4f35ba0b84a1d3d61555795
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-01-25 21:34:26 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -29,21 +29,17 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-// mikes code
-// router.post('/', (req, res) => {
-//   console.log(req.body)
-//   if (!req.body.pic) {
-//       req.body.pic = 'http://placekitten.com/400/400'
-//   }
-//   if (!req.body.city) {
-//       req.body.city = 'Anytown'
-//   }
-//   if (!req.body.state) {
-//       req.body.state = 'USA'
-//   }
-//   places.push(req.body)
-//   res.redirect('/places')
-// })
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]){
+    res.render('error404')
+  } else {
+    res.render('places/show', {place: places[id]})
+  }
+})
+
 
 // to import router to other files
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,11 +3,15 @@ const Def = require('../default')
 
 
 function index(data) {
-  let placesFormatted = data.places.map((place) => {
+  let placesFormatted = data.places.map((place, index) => {
     console.log(data)
     return (
       <div  className="col-sm-6">
-        <h2>{place.name}</h2>
+        <h2>
+          <a href={`/places/${index}`}>
+            {place.name} 
+          </a>
+        </h2>
         <p className="text-center">
           {place.cuisines}
         </p>
